70794ea1b9b1a994077d454777a7e45a1690263e
kusura-tech <litetsabr@gmail.com>
2026-08-14 12:34:29 +0300

    inst: angular material & integrating firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+
+        apiKey: "AIzaSyAp5MNCtgL4jUMYI_ygFIt3GGmeaBLf5C8",
+
+        authDomain: "kusura-technologies.firebaseapp.com",
+
+        projectId: "kusura-technologies",
+
+        storageBucket: "kusura-technologies.firebasestorage.app",
+
+        messagingSenderId: "585770053243",
+
+        appId: "1:585770053243:web:8bc480f7232121b6cafcde",
+
+        measurementId: "G-N2HLMJB9PV"
+
+    }
+}
